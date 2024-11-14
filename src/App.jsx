bc233a3f5de65af19e9bae4d595f12059796b376
anwarmohammadi1390 <anwarmohammadi1390@gmail.com>
2024-11-13 23:16:08 -0800

    setup the project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='text-center'>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis architecto minima deserunt ducimus reprehenderit, illum, consequatur ratione earum sequi dolore optio inventore, qui ipsa. Tempora eos totam quasi natus ex?</p>
+    </div>
+    </>
+  )
+}
+
+export default App
